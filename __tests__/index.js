577db9d3ea4c7dffa4a36430fa9b6c2fb200e0ce
Sean <sean.snow@live.com>
2015-12-28 10:14:01 +0800

    setStrField setField

--- __tests__/index.js
+++ __tests__/index.js
@@ -12,11 +12,11 @@ import ISOUtil from '../src/ISOUtil';
 let msg:ISOMsg = Packager.createISOMsg();
 
 msg.setMTI('0800');
-msg.setStrField(11, '000105');
-msg.setStrField(41, '00000764');
-msg.setStrField(42, '848100058126002');
-msg.setStrField(60, '00000001003');
-msg.setStrField(63, '001');
+msg.setField(11, '000105');
+msg.setField(41, '00000764');
+msg.setField(42, '848100058126002');
+msg.setField(60, '00000001003');
+msg.setField(63, '001');
 console.log(ISOUtil.hexString(msg.pack()));
 
 let msgStr = '08000020000000C00012000105303030303037363438343831303030353831323630303200110000000100300003303031';
