647620dc2f24cfb22708e653b37292b72877ec9b
Sean <sean.snow@live.com>
2017-03-07 15:06:09 +0800

    decodeLength 向下取整数

--- src/BcdPrefixer.js
+++ src/BcdPrefixer.js
@@ -11,12 +11,12 @@ class BcdPrefixer extends Prefixer {
 
     nDigits;
 
-    constructor(nDigits:Number) {
+    constructor(nDigits: Number) {
         super(nDigits);
         this.nDigits = nDigits;
     }
 
-    encodeLength(length:Number, b:Array) {
+    encodeLength(length: Number, b: Array) {
         for (let i = this.getPackedLength() - 1; i >= 0; i--) {
             let twoDigits = length % 100;
             length /= 100;
@@ -24,15 +24,15 @@ class BcdPrefixer extends Prefixer {
         }
     }
 
-    decodeLength(b, offset):Number {
+    decodeLength(b, offset): Number {
         let len = 0;
-        for (let i = 0; i < (this.nDigits + 1) / 2; i++) {
+        for (let i = 0; i < Math.floor((this.nDigits + 1) / 2); i++) {
             len = 100 * len + ((b[offset + i] & 0xF0) >> 4) * 10 + (b[offset + i] & 0x0F);
         }
         return len;
     }
 
-    getPackedLength():Number {
+    getPackedLength(): Number {
         return this.nDigits + 1 >> 1;
     }
 }
